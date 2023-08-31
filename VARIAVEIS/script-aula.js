06023ebba129ea223de000dd361d17a4177c267c
Diego Nonato <nonatodiego@hotmail.com>
2023-08-31 20:52:34 -0300

    aula 1 - variaveis

---
+++ VARIAVEIS/script-aula.js
@@ -0,0 +1,36 @@
+var nome = 'Diego';
+var idade = 23;
+var PossuiFaculdade = true;
+
+console.log(nome,idade, PossuiFaculdade);
+
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco)
+
+var sobrenome = 'Nonato', 
+    cidade = 'Rio de Janeiro';
+
+console.log(sobrenome,cidade);
+
+var semDefinir;
+
+console.log(semDefinir);
+
+// console.log(aindaNaoDefini);
+
+var comida;
+console.log(comida);
+var comida = 'Pizza';
+console.log(comida);
+
+// const time = 'Vasco';
+// time = 'Flamengo'
+// console.log(time);
+
+
+
+
---
+++ VARIAVEIS/script.js
@@ -0,0 +1,26 @@
+// Declarar uma variável com o seu nome
+
+var nome = 'Diego';
+
+// Declarar uma variável com a sua idade
+
+var idade = 27;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+
+comidaFavorita = 'Macarrão a bolonhesa'
+
+// Declarar 5 variáveis diferentes sem valores
+var time,
+    estado,
+    gostoMusical,
+    sexo,
+    altura;
+
+console.log(nome,idade,comidaFavorita);
+console.log(time,estado,gostoMusical,sexo,altura);
